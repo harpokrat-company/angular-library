1fffbf5c4b0ac51a319de1223da13058ce0b79b2
Antoine Stempfer <antoine.stempfer@boursorama.fr>
2019-05-03 17:04:27 +0200

    Using object for forRoot properties instead of inline properties

--- projects/harpokrat/src/lib/harpokrat.module.ts
+++ projects/harpokrat/src/lib/harpokrat.module.ts
@@ -3,7 +3,12 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 @NgModule()
 export class HarpokratModule {
 
-  static forRoot(serverUrl: string = 'https://api.harpokrat.com'): ModuleWithProviders {
+  static forRoot(
+    {
+      serverUrl = 'https://api.harpokrat.com'
+    }: {
+      serverUrl?: string
+    }): ModuleWithProviders {
     if (serverUrl.endsWith('/')) {
       serverUrl = serverUrl.substring(0, serverUrl.length - 1);
     }
